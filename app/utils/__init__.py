3a1d5f13945270f4ca1c2d15babdac1d700a869f
Muhammad-Husnain07 <muhammad.husnain.dev@gmail.com>
2026-03-27 15:31:14 +0500

    feat: add sha512_hexdigest, blake2b_hexdigest to hash_utils

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -13,7 +13,7 @@ from .input_sanitize import sanitize_input
 from .math_utils import clamp, percent, average, safe_divide, power, factorial, gcd, lcm
 from .int_utils import safe_int_parse, safe_float_parse, is_odd, is_even
 from .number_utils import is_numeric, round_to, is_positive, is_negative, is_zero, in_range
-from .hash_utils import sha256_hexdigest, md5_hexdigest, sha1_hexdigest
+from .hash_utils import sha256_hexdigest, md5_hexdigest, sha1_hexdigest, sha512_hexdigest, blake2b_hexdigest
 from .palindrome import is_palindrome
 from .json_utils import to_json, parse_json
 from .size_utils import is_large_file, bytes_to_human_readable, human_readable_to_bytes, bytes_to_kb, bytes_to_mb, format_bytes
@@ -34,7 +34,7 @@ __all__ = [
     "bytes_to_human_readable", "human_readable_to_bytes", "format_bytes",
     "get_utc_now", "add_days", "add_hours", "add_minutes", "add_seconds", "add_months", "add_weeks", "add_years", "is_expired", "time_until_expiry", "format_iso", "parse_iso", "days_between", "is_weekend", "is_weekday", "format_date", "seconds_to_hms", "parse_date", "get_quarter", "is_leap_year", "get_days_in_month", "is_same_day", "get_age", "format_duration", "time_ago",
     "slugify", "truncate", "truncate_words", "remove_extra_spaces", "extract_urls", "mask_email", "count_words", "count_chars", "count_vowels", "count_lines", "remove_html_tags", "reverse_string", "remove_special_chars", "starts_with", "ends_with", "contains_substring", "to_snake_case", "to_camel_case", "to_upper", "to_lower", "extract_numbers", "repeat_text", "join_with", "strip_punctuation",
-    "extract_domain", "sanitize_input", "clamp", "percent", "average", "safe_divide", "power", "factorial", "gcd", "lcm", "safe_int_parse", "safe_float_parse", "is_odd", "is_even", "normalize_extension", "sha256_hexdigest", "md5_hexdigest", "sha1_hexdigest", "is_palindrome", "to_json", "parse_json", "is_safe_url", "pretty_json", 
+    "extract_domain", "sanitize_input", "clamp", "percent", "average", "safe_divide", "power", "factorial", "gcd", "lcm", "safe_int_parse", "safe_float_parse", "is_odd", "is_even", "normalize_extension", "sha256_hexdigest", "md5_hexdigest", "sha1_hexdigest", "sha512_hexdigest", "blake2b_hexdigest", "is_palindrome", "to_json", "parse_json", "is_safe_url", "pretty_json", 
     "generate_random_string", "generate_random_hex", "generate_api_key", "generate_uuid", "generate_batch_id", "generate_token",
     "is_valid_url", "extract_domain", "extract_path", "extract_query_params", "build_url", "encode_params", "is_numeric", "round_to", "is_positive", "is_negative", "is_zero", "in_range", "count_vowels", "ConfigValidator", "count_lines", "is_empty", "flatten_list", "get_size", "ensure_int", "ensure_bool", "parse_bool", "ensure_float", "ensure_list", "sum_list", "uniq_list", "chunk_list", "min_list", "max_list", "flatten_list", "split_list", "find_duplicates", "format_number", "format_currency", "format_phone", "format_percentage", "format_ordinal"
 ]
--- app/utils/hash_utils.py
+++ app/utils/hash_utils.py
@@ -20,3 +20,17 @@ def sha1_hexdigest(text: str) -> str:
     if text is None:
         text = ""
     return hashlib.sha1(text.encode("utf-8")).hexdigest()
+
+
+def sha512_hexdigest(text: str) -> str:
+    """Return the SHA-512 hex digest for the given text."""
+    if text is None:
+        text = ""
+    return hashlib.sha512(text.encode("utf-8")).hexdigest()
+
+
+def blake2b_hexdigest(text: str) -> str:
+    """Return the BLAKE2b hex digest for the given text."""
+    if text is None:
+        text = ""
+    return hashlib.blake2b(text.encode("utf-8")).hexdigest()
